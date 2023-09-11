9edcd9c54ab3f8c90848f1c2a75c30403625ed60
skorokhodov <aaskorohodov@gmail.com>
2023-09-11 16:42:09 +0500

    Development

--- home/views.py
+++ home/views.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 class Home(TemplateView):
     """Home page"""
 
-    template_name = 'home/base.html'
+    template_name = 'home/home-content.html'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
