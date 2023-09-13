f9cb691671f76abd69e939577f68298011e1e86f
skorokhodov <aaskorohodov@gmail.com>
2023-09-13 15:20:16 +0500

    Save

---
+++ home/static/home/js/pop-up.js
@@ -0,0 +1,26 @@
+async function connectPopUp() {
+    let button = document.getElementById('season-3-bot');
+    button.addEventListener('click', openPopUp);
+
+    let closePopUpElement = document.getElementById("close-pop-up");
+    closePopUpElement.addEventListener("click", closePopUp);
+};
+
+function openPopUp() {
+    let popupContent = document.getElementById("popupContent");
+    let content = `
+    <p class="warning">Warning! NSFW! 18+!</p>
+    <p>Bot address - <a href="https://t.me/SggesterBot">https://t.me/SggesterBot</a></p>
+    <p>Bot password - free_invite</p>
+    `
+    popupContent.innerHTML = content;
+    document.getElementById("myModal").style.display = "block";
+};
+
+function closePopUp() {
+    let popupContent = document.getElementById("popupContent");
+    popupContent.innerHTML = '';
+    document.getElementById("myModal").style.display = "none";
+};
+
+connectPopUp();
